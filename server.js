130d2f5dedccba831424336f4303d018177cb4f7
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-30 16:05:57 -0400

    Created GET route /api/users to get all users

--- server.js
+++ server.js
@@ -5,16 +5,28 @@ server.use(express.json());
 
 server.use(logger);
 
+//users
+const userRouter = require('./users/userRouter.js')
+server.use('/api/users', userRouter)
+
+
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
+//Logger
 function logger(req, res, next) {
-console.log(req.method, req.originalUrl,new Date().toISOString())
-next();
+  console.log(req.method, req.originalUrl, new Date().toISOString())
+  next();
 };
+
+// function validaeUserId(req, res, next) {
+//   console.log()
+//   next();
+//   };
 
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,57 @@
-const express = 'express';
+const express = require('express');
+const userdb = require('./userDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+// router.post('/', (req, res) => {
 
-});
+// });
 
-router.post('/:id/posts', (req, res) => {
+// router.post('/:id/posts', (req, res) => {
 
-});
+// });
 
 router.get('/', (req, res) => {
-
+    userdb.get()
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: 'The user information could not be retrieved'
+            })
+        })
 });
 
-router.get('/:id', (req, res) => {
+// router.get('/:id', (req, res) => {
 
-});
+// });
 
-router.get('/:id/posts', (req, res) => {
+// router.get('/:id/posts', (req, res) => {
 
-});
+// });
 
-router.delete('/:id', (req, res) => {
+// router.delete('/:id', (req, res) => {
 
-});
+// });
 
-router.put('/:id', (req, res) => {
+// router.put('/:id', (req, res) => {
 
-});
+// });
 
-//custom middleware
+// //custom middleware
 
-function validateUserId(req, res, next) {
+// function validateUserId(req, res, next) {
 
-};
+// };
 
-function validateUser(req, res, next) {
+// function validateUser(req, res, next) {
 
-};
+// };
 
-function validatePost(req, res, next) {
+// function validatePost(req, res, next) {
 
-};
+// };
 
 module.exports = router;
