8c646b60198d3ca96653ae8fdaf9c732479faac5
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-30 17:10:19 -0400

    Created GET post for posts

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,13 +3,33 @@ const userdb = require('./userDb.js');
 
 const router = express.Router();
 
+//POST user
 // router.post('/', (req, res) => {
 
-// });
+//     const {name} = req.body;
+    
+//         userdb.insert(req.body)
+            
+//             .then(user => {
+//                 res.status(201).json(user);
+//             })
+//             .catch(err => {
+//                 console.log('error', err);
+//                 res.status(500).json({ error: "There was an error while saving the user to the database" })
+//             })
+    
+// })
 
-// router.post('/:id/posts', (req, res) => {
 
-// });
+router.get('/:id/posts', (req, res) => {
+const id = req.params.id;
+userdb.getUserPosts(id)
+.then(user => res.status(200).json(user))
+.catch(error => {
+    console.log(error);
+    res.status(500).json({error: "Error"})
+})
+});
 
 //returns list of users
 router.get('/', (req, res) => {
@@ -98,6 +118,7 @@ router.put('/:id', (req, res) => {
 // //custom middleware
 
 // function validateUserId(req, res, next) {
+    
 
 // };
 
